f26779b8c3e59101d31467d911416e24db378120
Nagasrinivasarao <140646635+Nagasrinivas9121@users.noreply.github.com>
2026-02-05 15:23:55 +0530

    changes in db

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = sequelize;
